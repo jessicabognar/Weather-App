556689b8ae7b42700fd583350246c27caca30748
jessicabognar <jessmbognar@gmail.com>
2021-10-03 13:55:41 -0400

    Updated CSS and design.

--- src/code.js
+++ src/code.js
@@ -29,7 +29,15 @@ dateElement.innerHTML = formatDate(currentTime);
 function formatDay(timestamp) {
   let date = new Date(timestamp * 1000);
   let day = date.getDay();
-  let days = ["Sun", "Mon", "Tues", "Wed", "Thurs", "Fri", "Sat"];
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
 
   return days[day];
 }
